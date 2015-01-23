988df342ab4902f627c6d1ca08eb65276926eeba
Philippe Isabelle <phil.isabelle@gmail.com>
2015-01-22 21:53:35 -0500

    Ajout d'un SOP de salutations

--- src/git/Git.java
+++ src/git/Git.java
@@ -15,6 +15,8 @@ public class Git {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Bonjour chéri!");    }
+        System.out.println("Bonjour chéri!");
+        System.out.println("Salut poulette");
+    }
     
 }
